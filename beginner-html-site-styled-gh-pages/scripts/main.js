50b9fb804eb273c5920bbea0b4b6a8d3937a6b2f
sparrow0hawk <alexjcoleman@gmail.com>
2019-10-26 21:53:56 +0100

    image change on click

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -6,3 +6,15 @@ let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!!';
 
 // single line comment
+
+// adding an image changer
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/duck.png');
+  } else {
+    myImage.setAttribute ('src', 'images/firefox-icon.png');
+  }
+}
