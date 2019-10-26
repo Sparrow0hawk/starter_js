1924e7d49ee3279d3c82bedb7cd5d737dcaddfec
sparrow0hawk <alexjcoleman@gmail.com>
2019-10-26 22:36:55 +0100

    what_is_js starter

---
+++ what_is_js/scripts/main.js
@@ -0,0 +1,9 @@
+// js for what is javascript test
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+  let name = prompt('Enter a new name');
+  para.textContent = 'Player 1: ' + name;
+}
