db31e9b9a5904a51eee092ee6df300ec6428155a
sparrow0hawk <alexjcoleman@gmail.com>
2019-10-26 22:05:44 +0100

    user prompt

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -2,8 +2,8 @@
 /*
 * wow a comment!
 */
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!!';
+//let myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!!';
 
 // single line comment
 
@@ -17,4 +17,26 @@ myImage.onclick = function() {
   } else {
     myImage.setAttribute ('src', 'images/firefox-icon.png');
   }
+}
+
+// personalised welcome message
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt("Enter name: ");
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
 }
