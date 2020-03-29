e22dae8e86fb82a7c746018ce19b41a6b8166af5
sparrow0hawk <alexjcoleman@gmail.com>
2020-03-29 21:37:09 +0100

    javascript clock first go

---
+++ js_clock/src/clock.js
@@ -0,0 +1,36 @@
+/*
+* clock javascript
+*/
+"use strict";
+
+function showTime() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  let time = h + ':' + m + ':' + s + ' ' + session;
+
+  document.getElementById("MyClockDisplay").innerText = time;
+
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  // recursively call the function every second
+  setTimeout(showTime, 1000)
+}
+
+showTime()
